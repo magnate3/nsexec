85b74da1f7ec8aa2e9361c131753c67ba3287c5c
magnate3 <liangeaglejun@126.com>
2020-12-07 16:25:32 +0800

    add nsexec demo

---
+++ git.sh
@@ -0,0 +1,7 @@
+echo "# nsexec" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/magnate3/nsexec.git
+git push -u origin main
